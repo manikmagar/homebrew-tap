967fbb875c9135c7497ae6a6187e7275c816843e
jreleaserbot <jreleaser@kordamp.org>
2022-10-11 02:39:14 +0000

    mulefd v0.8.4

--- Formula/mulefd.rb
+++ Formula/mulefd.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2022-10-11T01:42:31.85878807Z
+# Generated with JReleaser 1.2.0 at 2022-10-11T02:38:33.662478198Z
 class Mulefd < Formula
   desc "Generate flow diagrams for Mule applications"
   homepage "https://javastreets.com"
-  url "https://github.com/manikmagar/mulefd/releases/download/v0.8.2/mulefd-0.8.2.zip"
-  version "0.8.2"
-  sha256 "2e423df0eb3801c47b99c4ece2973546f40d184f802ea4cfc921efb241f5f5bf"
+  url "https://github.com/manikmagar/mulefd/releases/download/v0.8.4/mulefd-0.8.4.zip"
+  version "0.8.4"
+  sha256 "b2f392df91a173b942d4ddabc16becbe725a3c0ae86fca2582c0ae61000ed4b5"
   license "MIT"
 
 
@@ -15,6 +15,6 @@ class Mulefd < Formula
 
   test do
     output = shell_output("#{bin}/mulefd --version")
-    assert_match "0.8.2", output
+    assert_match "0.8.4", output
   end
 end
